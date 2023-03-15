606c7eab07eab44fd5c48118bfa217b25f0e8911
パク <ptn-kyungsuk.park@zozo.com>
2023-03-15 19:11:59 +0900

    Initial Commit

---
+++ SwiftUI_BreatheAnimation/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI_BreatheAnimation
+//
+//  Created by パク on 2023/03/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_BreatheAnimation/SwiftUI_BreatheAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BreatheAnimationApp.swift
+//  SwiftUI_BreatheAnimation
+//
+//  Created by パク on 2023/03/15.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BreatheAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
